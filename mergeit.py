5673c2ffa844cc0ce33de50f45ba9be475baf720
Sebastian Behrens <sebastian.tschen.behrens@gmail.com>
2024-01-26 23:18:26 +0100

    rearrange again

--- mergeit.py
+++ mergeit.py
@@ -15,7 +15,7 @@ with open(pdf_file,'rb') as input_file:
 
 
         # Scale and translate
-        op = Transformation().scale(sx=1.5, sy=1.5).translate(-125,-85)
+        op = Transformation().scale(sx=1.5, sy=1.5).translate(-155,-85)
         pdf_page.add_transformation(op)
 
         # add watermark
@@ -24,8 +24,8 @@ with open(pdf_file,'rb') as input_file:
         pdf_page.add_transformation(rot)
         # pdf_page.mediabox.upper_right = (pdf_page.mediabox.right*2,pdf_page.mediabox.top*2)
 
-        pdf_page.cropbox = RectangleObject([-290, 40, 0,500])
-        pdf_page.mediabox = RectangleObject([-290, 40, 0,500])
+        pdf_page.cropbox = RectangleObject([-290, 40, 0,470])
+        pdf_page.mediabox = RectangleObject([-290, 40, 0,470])
 
         output = PyPDF2.PdfWriter()
         output.add_page(pdf_page)
